a106a3c675f30114c5834fa660c7e7ddc39ab017
Cauã Graff <cauagraff@gmail.com>
2022-08-12 21:37:35 -0300

    foatorial

---
+++ 2 fase/Desenvolvimento de sistemas/Aula 2/Fatorial.java
@@ -0,0 +1,13 @@
+class Fatorial{
+    public int fatorial( int numero ) {
+        if( numero == 1){
+return
+        }
+         }
+
+   public static void main(String a[]){
+        Matematica mat = new Matematica();
+        System.out.println("resultado de 2*4: "+ mat.multiplica(2,4) );
+        
+   }
+}
